8958696f880d700982226054dc22570ce7a88c28
dipam-ag <106849090+dipam-ag@users.noreply.github.com>
2022-06-30 00:20:43 +0530

    uploaded files

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'this is a project developed by Dipam Agarwal from BIT MESRA Using HTML,CSS and JAVASCRIPT!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'this is a project developed by Dipam Agarwal from BIT MESRA Using HTML,CSS and JAVASCRIPT!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'this is a project developed by Dipam Agarwal from BIT MESRA Using HTML,CSS and JAVASCRIPT!',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'this is a project developed by Dipam Agarwal from BIT MESRA Using HTML,CSS and JAVASCRIPT!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'this is a project developed by Dipam Agarwal from BIT MESRA Using HTML,CSS and JAVASCRIPT!',
+        image: 'images/slider 5.PNG'
+    }
+]
